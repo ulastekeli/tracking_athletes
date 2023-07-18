d3e0ac7eee35103ba367e6d2112eaface7e570c4
Ulas Tekeli <tekeliulas@gmail.com>
2023-07-18 16:42:00 +0300

    feature aditions

--- app.cpp
+++ app.cpp
@@ -4,6 +4,7 @@
 #include <FL/Fl_Native_File_Chooser.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_PNG_Image.H>
+#include <FL/Fl_Int_Input.H>
 #include <FL/Fl_Check_Button.H>
 #include <FL/Fl_Output.H>  // For Fl_Output widget
 #include "Tracker.h"
@@ -17,35 +18,34 @@
 #include <thread>
 #include <chrono>
 
-
-void sleep(int seconds) {
-    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+void sleepFunction(int milliseconds) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
-
 
 std::string modelWeights = "../models/dev_models/pd_tiny4_best.weights";
 std::string modelConfiguration = "../models/dev_models/pd_tiny4.cfg";
-std::string reid_model_path = "../models/dep_models/osnet1.so";
+std::string reidModelPath = "../models/dep_models/osnet1.so";
 
 std::string detectionRoot = "../output/dets";
-std::string image_save_root = "../output/images";
-std::string cropped_save_root = "../output/cropped";
+std::string imageSaveRoot = "../output/images";
+std::string croppedSaveRoot = "../output/cropped";
 
 // Global color vector
 std::vector<cv::Scalar> colors;
-cv::Mat bg_img;
+cv::Mat bgImg;
 
-std::string selected_file_path = "../data/clip_tennis.mp4";  // To store the selected file path
-std::string model_option = "tennis";
-std::string inference_option = "tracking";
-bool show = true;
-bool save = true;
+std::string selectedFilePath = "../data/clip_tennis.mp4";  // To store the selected file path
+std::string modelOption = "tennis";
+std::string inferenceOption = "tracking";
+int fpsOption = 30;  // Default value
+bool showOption = true;
+bool saveOption = true;
 
 class VideoProcessor
 {
 public:
     VideoProcessor(Fl_Box *box) : box(box) {}
-    void process_video(std::string video_path, std::string inference_option);
+    void processVideo(std::string videoPath, std::string inferenceOption);
     void displayTracks(std::string videoPath);
     void getCroppedImages(std::string videoPath);
 
@@ -54,7 +54,7 @@ private:
 };
 
 
-void generateColors(int num_colors)
+void generateColors(int numColors)
 {
     // Initialize the random number generator
     std::random_device rd;
@@ -62,8 +62,8 @@ void generateColors(int num_colors)
     std::uniform_int_distribution<int> uni(0, 255);
 
     // Generate the colors
-    colors.resize(num_colors);
-    for (int i = 0; i < num_colors; ++i)
+    colors.resize(numColors);
+    for (int i = 0; i < numColors; ++i)
     {
         int r = uni(rng);
         int g = uni(rng);
@@ -79,21 +79,21 @@ cv::Scalar getIdColor(int id)
     return colors[id % colors.size()];
 }
 
-void create_directory(const std::string &dir_name)
+void createDirectory(const std::string &dirName)
 {
-    std::filesystem::path dir_path(dir_name);
-    if (!std::filesystem::exists(dir_path))
+    std::filesystem::path dirPath(dirName);
+    if (!std::filesystem::exists(dirPath))
     {
-        std::filesystem::create_directory(dir_path);
-        std::cout << "Directory created: " << dir_path << std::endl;
+        std::filesystem::create_directory(dirPath);
+        std::cout << "Directory created: " << dirPath << std::endl;
     }
     else
     {
-        std::cout << "Directory already exists: " << dir_path << std::endl;
+        std::cout << "Directory already exists: " << dirPath << std::endl;
     }
 }
 
-void button_cb(Fl_Widget* btn, void* userdata) {
+void buttonCb(Fl_Widget* btn, void* userdata) {
     Fl_Native_File_Chooser chooser;
     chooser.title("Pick a file");
     chooser.type(Fl_Native_File_Chooser::BROWSE_FILE);
@@ -101,66 +101,77 @@ void button_cb(Fl_Widget* btn, void* userdata) {
     chooser.directory("../data"); // replace with your directory path
 
     // Update modelWeights and modelConfiguration based on model_option
-    if (model_option == "football") {
+    if (modelOption == "football") {
         modelWeights = "../models/dev_models/pd_tiny4_best.weights";
         modelConfiguration = "../models/dev_models/pd_tiny4.cfg";
-    } else if (model_option == "tennis") {
+    } else if (modelOption == "tennis") {
         modelWeights = "../models/dev_models/pd_tiny4_tennis.weights";
         modelConfiguration = "../models/dev_models/pd_tiny4_tennis.cfg";
     }
 
     if (chooser.show() == 0)
     {
-        selected_file_path = chooser.filename();
+        selectedFilePath = chooser.filename();
         Fl_Output* output = (Fl_Output*)userdata;
-        output->value(selected_file_path.c_str());
+        output->value(selectedFilePath.c_str());
     }
 
 }
 
-void start_cb(Fl_Widget* btn, void* userdata) {
+// Callback for FPS input
+void fpsInputCb(Fl_Widget* w, void* userdata) {
+    Fl_Int_Input* input = (Fl_Int_Input*)w;
+    fpsOption = std::stoi(input->value());
+}
+
+void displayCb(Fl_Widget* btn, void* userdata) {
     VideoProcessor *processor = static_cast<VideoProcessor *>(userdata);
-    processor->process_video(selected_file_path, inference_option);
+    processor->displayTracks(selectedFilePath);
+}
+
+void startCb(Fl_Widget* btn, void* userdata) {
+    VideoProcessor *processor = static_cast<VideoProcessor *>(userdata);
+    processor->processVideo(selectedFilePath, inferenceOption);
 }
 
 // Callback for model choice
-void model_choice_cb(Fl_Widget* w, void* userdata) {
+void modelChoiceCb(Fl_Widget* w, void* userdata) {
     Fl_Choice* choice = (Fl_Choice*)w;
-    model_option = choice->text();
+    modelOption = choice->text();
 }
 
 // Callback for inference choice
-void inference_choice_cb(Fl_Widget* w, void* userdata) {
+void inferenceChoiceCb(Fl_Widget* w, void* userdata) {
     Fl_Choice* choice = (Fl_Choice*)w;
-    inference_option = choice->text();
+    inferenceOption = choice->text();
 }
 
 // Callback for when the check button is clicked
-void check_cb(Fl_Widget* btn, void* userdata) {
+void checkCb(Fl_Widget* btn, void* userdata) {
     Fl_Check_Button* check = static_cast<Fl_Check_Button*>(btn);
-    show = check->value();
+    showOption = check->value();
 }
 
-Fl_RGB_Image *CopyMat2FlImage(const cv::Mat &mat)
+Fl_RGB_Image *copyMat2FlImage(const cv::Mat &mat)
 {
-    cv::Mat mat_converted;
-    cv::cvtColor(mat, mat_converted, cv::COLOR_BGR2RGB);
-    uchar *buffer = new uchar[mat_converted.rows * mat_converted.cols * mat_converted.channels()];
-    std::memcpy(buffer, mat_converted.data, mat_converted.rows * mat_converted.cols * mat_converted.channels());
-    return new Fl_RGB_Image(buffer, mat_converted.cols, mat_converted.rows, 3);
+    cv::Mat matConverted;
+    cv::cvtColor(mat, matConverted, cv::COLOR_BGR2RGB);
+    uchar *buffer = new uchar[matConverted.rows * matConverted.cols * matConverted.channels()];
+    std::memcpy(buffer, matConverted.data, matConverted.rows * matConverted.cols * matConverted.channels());
+    return new Fl_RGB_Image(buffer, matConverted.cols, matConverted.rows, 3);
 }
 
 void VideoProcessor::getCroppedImages(std::string videoPath)
 {
     generateColors(100);
     std::filesystem::path path(videoPath);
-    std::string video_name = path.stem();
+    std::string videoName = path.stem();
 
     // Append the video filename to the detectionPath
-    std::string video_detection_path = detectionRoot + "/" + video_name;
-    std::string cropped_save_path = cropped_save_root + "/" + video_name;
+    std::string videoDetectionPath = detectionRoot + "/" + videoName;
+    std::string croppedSavePath = croppedSaveRoot + "/" + videoName;
 
-    create_directory(cropped_save_path);
+    createDirectory(croppedSavePath);
     cv::VideoCapture cap(videoPath);
     if (!cap.isOpened())
     {
@@ -182,10 +193,10 @@ void VideoProcessor::getCroppedImages(std::string videoPath)
             break;
         std::cout << "Displaying frame " << frameId << std::endl;
         // Open the corresponding text file
-        std::ifstream inFile(video_detection_path + "/" + std::to_string(frameId) + ".txt");
+        std::ifstream inFile(videoDetectionPath + "/" + std::to_string(frameId) + ".txt");
         if (!inFile)
         {
-            std::cerr << "ERROR: Can't open file " << video_detection_path + "/" + std::to_string(frameId) + ".txt" << std::endl;
+            std::cerr << "ERROR: Can't open file " << videoDetectionPath + "/" + std::to_string(frameId) + ".txt" << std::endl;
             return;
         }
 
@@ -204,7 +215,7 @@ void VideoProcessor::getCroppedImages(std::string videoPath)
 
             cv::Rect roi(xmin, ymin, xmax - xmin, ymax - ymin);                                                          // Define the region of interest
             cv::Mat cropped = frame(roi);                                                                                // Crop the image
-            cv::imwrite(cropped_save_root + "/" + std::to_string(frameId) + "_" + std::to_string(id) + ".png", cropped); // Save the cropped image
+            cv::imwrite(croppedSaveRoot + "/" + std::to_string(frameId) + "_" + std::to_string(id) + ".png", cropped); // Save the cropped image
         }
 
         frameId++;
@@ -216,13 +227,14 @@ void VideoProcessor::getCroppedImages(std::string videoPath)
 void VideoProcessor::displayTracks(std::string videoPath)
 {
     std::filesystem::path path(videoPath);
-    std::string video_name = path.stem();
+    std::string videoName = path.stem();
 
     // Append the video filename to the detectionPath
-    std::string video_detection_path = detectionRoot + "/" + video_name;
-    std::string image_save_path = image_save_root + "/" + video_name;
-    create_directory(image_save_path);
+    std::string videoDetectionPath = detectionRoot + "/" + videoName;
+    std::string imageSavePath = imageSaveRoot + "/" + videoName;
+    createDirectory(imageSavePath);
     cv::VideoCapture cap(videoPath);
+    int intervalMs = static_cast<int>(1000. / fpsOption);
 
     int frameId = 0;
     cv::Mat frame;
@@ -233,10 +245,10 @@ void VideoProcessor::displayTracks(std::string videoPath)
         if (frame.empty())
             break;
 
-        std::ifstream inFile(video_detection_path + "/" + std::to_string(frameId) + ".txt");
+        std::ifstream inFile(videoDetectionPath + "/" + std::to_string(frameId) + ".txt");
         if (!inFile)
         {
-            std::cerr << "ERROR: Can't open file " << video_detection_path + "/" + std::to_string(frameId) + ".txt" << std::endl;
+            std::cerr << "ERROR: Can't open file " << videoDetectionPath + "/" + std::to_string(frameId) + ".txt" << std::endl;
             return;
         }
 
@@ -248,23 +260,30 @@ void VideoProcessor::displayTracks(std::string videoPath)
 
             if (!(iss >> id >> xmin >> ymin >> xmax >> ymax))
             {
-                std::cerr << "ERROR: Can't parse line " << line << " in file " << frameId << ".txt" << std::endl;
-                return;
+                iss.clear(); // Clear the error state
+                iss.str(line);
+                if (!(iss >> xmin >> ymin >> xmax >> ymax)){
+                    std::cerr << "ERROR: Can't parse line " << line << " in file " << frameId << ".txt" << std::endl;
+                    return;
+                }
+                id = -1;
             }
-
-            cv::rectangle(frame, cv::Point(xmin, ymin), cv::Point(xmax, ymax), getIdColor(id), 2);
-
-            std::string label = std::to_string(id);
-            int baseline;
-            cv::Size labelSize = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 1, 1, &baseline);
-            cv::putText(frame, label, cv::Point(xmin, ymin + labelSize.height), cv::FONT_HERSHEY_SIMPLEX, 1, getIdColor(id), 2);
+            if(id != -1){
+                cv::rectangle(frame, cv::Point(xmin, ymin), cv::Point(xmax, ymax), getIdColor(id), 2);
+                std::string label = std::to_string(id);
+                int baseline;
+                cv::Size labelSize = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 1, 1, &baseline);
+                cv::putText(frame, label, cv::Point(xmin, ymin + labelSize.height), cv::FONT_HERSHEY_SIMPLEX, 1, getIdColor(id), 2);
+            }
+            else{
+                cv::rectangle(frame, cv::Point(xmin, ymin), cv::Point(xmax, ymax), getIdColor(0), 2);
+            }
         }
-
-        if (show)
+        if (showOption)
         {
             cv::Mat resized;
             cv::resize(frame, resized, cv::Size(box->w(), box->h()));
-            Fl_RGB_Image *newImage = CopyMat2FlImage(resized);
+            Fl_RGB_Image *newImage = copyMat2FlImage(resized);
             delete (Fl_RGB_Image *)box->image();
             box->image(newImage);
             box->redraw();
@@ -272,36 +291,37 @@ void VideoProcessor::displayTracks(std::string videoPath)
         }
         else
         {
-            cv::imwrite(image_save_path + "/" + std::to_string(frameId) + ".jpg", frame);
+            cv::imwrite(imageSavePath + "/" + std::to_string(frameId) + ".jpg", frame);
         }
 
         frameId++;
+        sleepFunction(intervalMs);
     }
 
     cap.release();
 }
 
-void VideoProcessor::process_video(std::string video_path, std::string inference_option)
+void VideoProcessor::processVideo(std::string videoPath, std::string inferenceOption)
 {
-    std::cout<<inference_option<<std::endl;
+    std::cout<<inferenceOption<<std::endl;
     std::cout<<modelWeights<<std::endl;
-    std::cout<<video_path<<std::endl;
+    std::cout<<videoPath<<std::endl;
 
-    sleep(2);
+    sleepFunction(1000);
     ObjectDetector detector(modelWeights, modelConfiguration);
-    Tracker tracker(reid_model_path);
-    cv::VideoCapture cap(video_path);
+    Tracker tracker(reidModelPath);
+    cv::VideoCapture cap(videoPath);
 
     // Extract the video filename from the path
-    std::filesystem::path path(video_path);
-    std::string video_name = path.stem();
+    std::filesystem::path path(videoPath);
+    std::string videoName = path.stem();
 
     // Append the video filename to the detectionPath
-    std::string video_detection_path = detectionRoot + "/" + video_name;
-    std::string image_save_path = image_save_root + "/" + video_name;
+    std::string videoDetectionPath = detectionRoot + "/" + videoName;
+    std::string imageSavePath = imageSaveRoot + "/" + videoName;
 
-    create_directory(video_detection_path);
-    create_directory(image_save_path);
+    createDirectory(videoDetectionPath);
+    createDirectory(imageSavePath);
 
     int frameNumber = 0;
     cv::Mat frame;
@@ -317,23 +337,23 @@ void VideoProcessor::process_video(std::string video_path, std::string inference
 
         std::ofstream outFile;
         std::stringstream ss;
-        ss << video_detection_path << "/" << frameNumber << ".txt";
+        ss << videoDetectionPath << "/" << frameNumber << ".txt";
         outFile.open(ss.str());
 
-        // Distinguish between detection and tracking results based on inference_option
-        if (inference_option == "detection only") {
+        // Distinguish between detection and tracking results based on inferenceOption
+        if (inferenceOption == "detection only") {
             for(const auto& box : detectedObjects) {
                 outFile << box.xmin << " " << box.ymin << " " << box.xmax << " " << box.ymax << std::endl;
-                if (show || save) {
+                if (showOption || saveOption) {
                     cv::rectangle(frame, cv::Point(box.xmin, box.ymin), cv::Point(box.xmax, box.ymax), cv::Scalar(255, 0, 0), 2);
                 }
             }
-        } else if (inference_option == "tracking") {
+        } else if (inferenceOption == "tracking") {
             for (const auto &track : tracker.getLastFrameTracks())
             {
                 const auto &box = track.box_history.back();
                 outFile << track.id << " " << box.xmin << " " << box.ymin << " " << box.xmax << " " << box.ymax << std::endl;
-                if (show || save)
+                if (showOption || saveOption)
                 {
                     cv::rectangle(frame, cv::Point(box.xmin, box.ymin), cv::Point(box.xmax, box.ymax), getIdColor(track.id), 2);
 
@@ -346,28 +366,28 @@ void VideoProcessor::process_video(std::string video_path, std::string inference
         }
 
         outFile.close();
-        if (show)
+        if (showOption)
         {
             cv::Mat resized;
             cv::resize(frame, resized, cv::Size(box->w(), box->h()));
-            Fl_RGB_Image *newImage = CopyMat2FlImage(resized);
+            Fl_RGB_Image *newImage = copyMat2FlImage(resized);
             delete (Fl_RGB_Image *)box->image();
             box->image(newImage);
             box->redraw();
             Fl::check();
         }
-        if (save)
+        if (saveOption)
         {
-            cv::imwrite(image_save_path + "/" + std::to_string(frameNumber) + ".jpg", frame);
+            cv::imwrite(imageSavePath + "/" + std::to_string(frameNumber) + ".jpg", frame);
         }
         frameNumber++;
     }
     cap.release();
-    Fl_RGB_Image *tmp = CopyMat2FlImage(bg_img);
-    delete (Fl_RGB_Image *)box->image();
-    box->image(tmp);
-    box->redraw();
-    Fl::check();
+    // Fl_RGB_Image *tmp = copyMat2FlImage(bgImg);
+    // delete (Fl_RGB_Image *)box->image();
+    // box->image(tmp);
+    // box->redraw();
+    // Fl::check();
 }
 
 int main(int argc, char **argv)
@@ -380,36 +400,44 @@ int main(int argc, char **argv)
 
     Fl_Output* output = new Fl_Output(80, 80, 200, 25);  // New output widget to display the selected file path
 
-    Fl_Choice* model_choice = new Fl_Choice(80, 140, 200, 25, "Model:");
-    model_choice->add("football model");
-    model_choice->add("tennis model");
-    model_choice->value(1);
-    model_choice->callback(model_choice_cb);
-    model_option = "tennis";
+    Fl_Choice* modelChoice = new Fl_Choice(80, 140, 200, 25, "Model:");
+    modelChoice->add("football model");
+    modelChoice->add("tennis model");
+    modelChoice->value(1);
+    modelChoice->callback(modelChoiceCb);
+    modelOption = "tennis";
     
 
-    Fl_Choice* inference_choice = new Fl_Choice(80, 200, 200, 25, "Inference:");
-    inference_choice->add("detection only");
-    inference_choice->add("tracking");
-    inference_choice->value(1);  // Set the default choice to "tracking"
-    inference_choice->callback(inference_choice_cb);
-    inference_option = "tracking";
+    Fl_Choice* inferenceChoice = new Fl_Choice(80, 200, 200, 25, "Inference:");
+    inferenceChoice->add("detection only");
+    inferenceChoice->add("tracking");
+    inferenceChoice->value(1);  // Set the default choice to "tracking"
+    inferenceChoice->callback(inferenceChoiceCb);
+    inferenceOption = "tracking";
 
     Fl_Check_Button *check = new Fl_Check_Button(80, 240, 300, 50, "Show while processing");
     check->value(1);  // Set the initial state to ticked
-    check->callback(check_cb, nullptr);
+    check->callback(checkCb, nullptr);
+    
+    // Add an FPS input field
+    Fl_Int_Input* fpsInput = new Fl_Int_Input(80, 300, 200, 25, "FPS:");
+    fpsInput->value(std::to_string(fpsOption).c_str());  // Set the default value to 30
+    fpsInput->callback(fpsInputCb);
 
-    Fl_Button *start_button = new Fl_Button(80, 300, 200, 50, "Start");  // New start button
+    Fl_Button *displayButton = new Fl_Button(80, 340, 200, 50, "Display Detections/Tracks");  // New start button
+    Fl_Button *startButton = new Fl_Button(80, 400, 200, 50, "Start");  // New start button
+
 
     Fl_Box *box = new Fl_Box(400, 20, 1360, 960);
     cv::Mat img = cv::imread("../data/example.jpg");
     cv::resize(img, img, cv::Size(1360, 940));
-    Fl_RGB_Image *tmp = CopyMat2FlImage(img);
+    Fl_RGB_Image *tmp = copyMat2FlImage(img);
     box->image(tmp);
 
     VideoProcessor processor(box);
-    button->callback(button_cb, output);
-    start_button->callback(start_cb, &processor); 
+    button->callback(buttonCb, output);
+    displayButton->callback(displayCb, &processor); 
+    startButton->callback(startCb, &processor); 
 
     window->end();
     window->show(argc, argv);
